e0bf3d71e6a3446234280e7e311a6e2faaeaad19
Oskar Petersons <a.more.reliable.source@gmail.com>
2014-10-16 21:28:20 -0400

    layout adjustment

--- js/treeVis.js
+++ js/treeVis.js
@@ -51,7 +51,7 @@
     }
 
     var diagonal = d3.svg.diagonal()        //used in making lines in the tree layout.
-        .projection(function (d) {return [d.x, d.y+60]; });
+        .projection(function (d) {return [d.x, d.y+65]; });
 
     var refresh = function () {
         d3.selectAll("circle").style("fill", "lightBlue")
@@ -92,7 +92,7 @@
 
         visNodes.attr("class", function (d) {return "node st" + d.dat.id; })
             .attr("cx", function (d) {return d.x; })
-            .attr("cy", function (d) {return d.y + 60; });
+            .attr("cy", function (d) {return d.y + 65; });
 
         visLabels.exit().remove();
         visLabels.enter()
@@ -101,7 +101,7 @@
             .attr("dy", 3);
         visLabels.text(function (d) {return d.dat.ordered; })
             .attr("transform", function (d) {
-                return "translate(" + (d.x) + "," + (d.y + 60) + ")";
+                return "translate(" + (d.x) + "," + (d.y + 65) + ")";
             });
 
     };
@@ -128,7 +128,7 @@
     treeVis.refresh = refresh;
     
     treeVis.resize = function () {
-        tree.size([container.style("width").slice(0, -2), container.style("height").slice(0, -2) - 100]);
+        tree.size([container.style("width").slice(0, -2), container.style("height").slice(0, -2) - 150]);
         refresh();
     }
     
